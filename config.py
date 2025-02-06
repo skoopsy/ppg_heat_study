5ab74ebf486dd700f577387636b9d0006db4f1a7
Dave <38195019+skoopsy@users.noreply.github.com>
2025-02-06 14:36:20 +0000

    config for load

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ SENSOR_TYPES= {
 CONDITIONS = ["pre_heat_exposure", "intra_heat_exposure", "post_heat_exposure"]
 
 # For checkpointing
-LOAD_CHECKPOINT = False
-SAVE_CHECKPOINT = True
+LOAD_CHECKPOINT = True
+SAVE_CHECKPOINT = False
 CHECKPOINT_FILE = "data/pickled/ID00_loaded_data_000.pkl"
 CHECKPOINT_ID = 0
 
